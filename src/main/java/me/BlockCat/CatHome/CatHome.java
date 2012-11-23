37893c4253a3c9614f5ec3962dda1920d9a586d7
Zino <Zino@192.168.0.213>
2012-11-23 21:46:58 +0100

    +Initial commit
    //Using maven for the first time, no idea what I'm doing.

---
+++ src/main/java/me/BlockCat/CatHome/CatHome.java
@@ -0,0 +1,7 @@
+package me.BlockCat.CatHome;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CatHome extends JavaPlugin {
+
+}
---
+++ src/main/java/me/BlockCat/CatHome/Saver.java
@@ -0,0 +1,13 @@
+package me.BlockCat.CatHome;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class Saver {
+	
+	
+	public void saveHome(Player player, String name, Location location) {
+		
+	}
+
+}
